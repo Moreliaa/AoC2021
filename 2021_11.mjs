3f51c248d9486d4a234581b723ac8a32e5a85d38
Eric E <eth_er@gmx.de>
2021-12-11 12:49:55 +0100

    day11

--- 2021_11.mjs
+++ 2021_11.mjs
@@ -1,16 +1,6 @@
 import Util from './util.mjs'
 
 export function solve(input) {
-    /*input =`5483143223
-2745854711
-5264556173
-6141336146
-6357385478
-4167524645
-2176841721
-6882881134
-4846848554
-5283751526`*/
     let map = Util.splitLines(input).map(l => l.split('').map(n => parseInt(n, 10)))
 
     let steps = 0
