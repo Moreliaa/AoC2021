cee6d4562342f945c745bfa564f61d40e55e80f4
Eric E <eth_er@gmx.de>
2021-12-17 20:47:20 +0100

    day17 ref

--- 2021_17.mjs
+++ 2021_17.mjs
@@ -35,25 +35,12 @@ function attempt(v_x, v_y, xMin, xMax, yMin, yMax) {
         y = result[1]
         max_y_reached = Math.max(y, max_y_reached)
         if (x >= xMin && x <= xMax && y >= yMin && y <= yMax) {
-            //printStuff(set, xMin, xMax, yMin, yMax, x, max_y_reached, y)
             return [true, max_y_reached]
         }
         v_x = result[2]
         v_y = result[3]
     }
-    //printStuff(set, xMin, xMax, yMin, yMax, x, max_y_reached, y)
     return [false, 0]
-}
-
-function printStuff(set, xMin, xMax, yMin, yMax, max_x_reached, max_y_reached, yEnd) {
-    /*for (let y = max_y_reached; y >= yEnd; y--) {
-        let line = ""
-        for (let x = 0; x <= max_x_reached; x++) {
-            line += set.has(Util.map_getKey(x, y)) ? "#" : (x >= xMin && x <= xMax && y >= yMin && y <= yMax) ? "T" : "."
-        }
-        console.log(line)
-    }*/
-    console.log(max_x_reached, max_y_reached, yEnd)
 }
 
 function step(x, y, v_x, v_y) {
