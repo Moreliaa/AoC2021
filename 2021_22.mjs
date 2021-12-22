24437d43caadd2ffa9c95c16dbe0093ca9ce4798
Moreliaa <eth_er@gmx.de>
2021-12-23 00:50:49 +0100

    day22 wip

--- 2021_22.mjs
+++ 2021_22.mjs
@@ -20,12 +20,14 @@ on x=-16..35,y=-41..10,z=-47..6
 off x=-32..-23,y=11..30,z=-14..3
 on x=-49..-5,y=-3..45,z=-29..18
 off x=18..30,y=-20..-8,z=-3..13
-on x=-41..9,y=-7..43,z=-33..15`
-
-input = `on x=10..12,y=10..12,z=10..12
+on x=-41..9,y=-7..43,z=-33..15
+on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
+on x=967..23432,y=45373..81175,z=27513..53682`
+/*
+    input = `on x=10..12,y=10..12,z=10..12
 on x=11..13,y=11..13,z=11..13
 off x=9..11,y=9..11,z=9..11
-on x=10..10,y=10..10,z=10..10`
+on x=10..10,y=10..10,z=10..10`*/
     let lines = Util.splitLines(input)
     let rx = new RegExp(/(.+) x=(.+)\.\.(.+),y=(.+)\.\.(.+),z=(.+)\.\.(.+)/)
     let commands = lines.map(l => {
@@ -40,12 +42,13 @@ on x=10..10,y=10..10,z=10..10`
         }
     })
 
-    let pt1;
+    let pt1 = 0;
     let cubes = []
     let steps = 0
     for (let command of commands) {
+        console.log(command)
         steps++
-        if (!pt1 && command.xMin > 50 && command.xMax < 50)
+        if (pt1 === 0 && Math.abs(command.dim.xMin) > 50)
             pt1 = onCubes(cubes)
         cubes = step(cubes, command)
         console.log(steps, onCubes(cubes))
@@ -68,17 +71,21 @@ function step(cubes, command) {
                 newCubes.push(c)
                 commandCubes = splitCubes(commandCubes, c)
             } else {
-                newCubes.push(splitCubes([c], command.dim))
+                newCubes = newCubes.concat(splitCubes([c], command.dim))
             }
+        } else {
+            newCubes.push(c)
         }
     }
-    return newCubes.concat(commandCubes)
+    if (command.op === 'on')
+        newCubes = newCubes.concat(commandCubes)
+    return newCubes
 }
 
 function isIntersection(cube1, cube2) {
-    return  checkAxis(cube1.xMin, cube1.xMax, cube2.xMin, cube2.xMax) &&
-            checkAxis(cube1.yMin, cube1.yMax, cube2.yMin, cube2.yMax) &&
-            checkAxis(cube1.zMin, cube1.zMax, cube2.zMin, cube2.zMax)
+    return checkAxis(cube1.xMin, cube1.xMax, cube2.xMin, cube2.xMax) &&
+        checkAxis(cube1.yMin, cube1.yMax, cube2.yMin, cube2.yMax) &&
+        checkAxis(cube1.zMin, cube1.zMax, cube2.zMin, cube2.zMax)
 }
 
 function checkAxis(min1, max1, min2, max2) {
@@ -87,6 +94,8 @@ function checkAxis(min1, max1, min2, max2) {
 
 function splitCubes(cubesToSplit, intersectingCube) {
     let newCubes = []
+    console.log("splitentry", cubesToSplit, intersectingCube)
+    console.log("on1", onCubes(cubesToSplit))
     for (let c of cubesToSplit) {
         if (!isIntersection(c, intersectingCube))
             newCubes.push(c)
@@ -94,6 +103,7 @@ function splitCubes(cubesToSplit, intersectingCube) {
             newCubes = newCubes.concat(split(c, intersectingCube))
         }
     }
+    console.log("on2", onCubes(newCubes))
     console.log("Split result", newCubes)
     return newCubes
 }
@@ -101,34 +111,43 @@ function splitCubes(cubesToSplit, intersectingCube) {
 function split(c, intersectingCube) {
     // remove intersecting area from c
     let newCubes = []
-    for (let axis of ['x','y','z']) {
-        let subCube = splitAxis(axis, c, intersectingCube)
-        if (!subCube)
-            continue
-            if (c[axis + 'Max'] !== subCube[axis + 'Max'])
-                c[axis + 'Min'] = subCube[axis + 'Max'] + 1
-            else if (c[axis + 'Min'] !== subCube[axis + 'Min'])
-                c[axis + 'Max'] = subCube[axis + 'Min'] - 1
-            newCubes.push(subCube)
+    for (let axis of ['x', 'y', 'z']) {
+        newCubes = newCubes.concat(splitAxis(axis, c, intersectingCube))
+        //console.log("2", c)
     }
     return newCubes
 }
 
 function splitAxis(axis, c, intersectingCube) {
+    let newCubes = []
     let minKey = axis + 'Min'
     let maxKey = axis + 'Max'
-    if (c[minKey] >= intersectingCube[minKey] && c[maxKey] <= intersectingCube[maxKey])
-        return null // this axis is fully covered by the intersecting cube so volume is zero
-    let min_new = c[minKey] <= intersectingCube[minKey] ? c[minKey] : intersectingCube[maxKey]
-    let max_new = c[maxKey] >= intersectingCube[minKey] ? c[maxKey] : intersectingCube[minKey]
-    let newCube = {}
-    newCube[minKey] = min_new
-    newCube[maxKey] = max_new
-    for (let a of ['x', 'y', 'z']) {
-        if (a !== axis) {
-            newCube[a + 'Min'] = c[a + 'Min']
-            newCube[a + 'Max'] = c[a + 'Max']
+    if (c[minKey] < intersectingCube[minKey]) {
+        let newCube = {}
+        newCube[minKey] = c[minKey]
+        newCube[maxKey] = intersectingCube[minKey] - 1
+        for (let a of ['x', 'y', 'z']) {
+            if (a !== axis) {
+                newCube[a + 'Min'] = c[a + 'Min']
+                newCube[a + 'Max'] = c[a + 'Max']
+            }
         }
+        newCubes.push(newCube)
+        c[minKey] = Math.min(intersectingCube[minKey], c[maxKey])
     }
-    return newCube
+
+    if (c[maxKey] > intersectingCube[maxKey]) {
+        let newCube = {}
+        newCube[minKey] = intersectingCube[maxKey] +1
+        newCube[maxKey] = c[maxKey]
+        for (let a of ['x', 'y', 'z']) {
+            if (a !== axis) {
+                newCube[a + 'Min'] = c[a + 'Min']
+                newCube[a + 'Max'] = c[a + 'Max']
+            }
+        }
+        newCubes.push(newCube)
+        c[maxKey] = Math.max(intersectingCube[maxKey], c[minKey])
+    }
+    return newCubes
 }
